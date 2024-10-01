f56d0b62476cdc20d5159b07eec5384a3af39105
Amr-Dweikat939 <amrodwaikat@gmail.com>
2024-10-01 19:36:13 +0300

    add dark mode

---
+++ index.js
@@ -0,0 +1,16 @@
+let darkMode = true;
+function changeMode() {
+  let root = document.querySelector(":root");
+  if (darkMode) {
+    root.style.setProperty("--bg_default", "#1A1A1A");
+    root.style.setProperty("--bg_body", "#282828");
+    root.style.setProperty("--lines-color", "#000000");
+    root.style.setProperty("--body-text", "#EDEDED");
+  } else {
+    root.style.setProperty("--bg_default", "#FFFFFF");
+    root.style.setProperty("--bg_body", "#F0F9FF");
+    root.style.setProperty("--lines-color", "#DDDDDD");
+    root.style.setProperty("--body-text", "#333333");
+  }
+  darkMode = !darkMode;
+}
